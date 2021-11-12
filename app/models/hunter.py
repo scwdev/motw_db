8b404a68b33dbf708e7a4b919a44decc37f62df8
Noam Blanks <noamblanks@gmail.com>
2021-11-12 12:41:34 -0500

    association tables started

--- app/models/hunter.py
+++ app/models/hunter.py
@@ -1,16 +1,36 @@
-from sqlalchemy import Boolean, Column, ForeignKey, String, Integer
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, ForeignKey, String, Integer, Text
+from sqlalchemy.orm import relation, relationship
 
 from app.database import Base, ID_and_Timestamps
+from .association_tables import hunters_to_moves, hunters_to_gear
 
 
 class Hunter(Base, ID_and_Timestamps):
     __tablename__ = "hunters"
 
-    user_id = Column(Integer, ForeignKey('user.id'))
+    # many-to-one
+    user_id = Column(Integer, ForeignKey('users.id'))
+    playbook_id = Column(Integer, ForeignKey('playbooks.id'))
+    
+    # many-to-many
+    moves = relationship("Move", secondary=hunters_to_moves)
+    gear = relationship("Gear", secondary=hunters_to_gear)
 
     name = Column(String, nullable=False)
-    playbook = relationship("Playbook")
+    charm = Column(Integer)
+    cool = Column(Integer) 
+    sharp = Column(Integer) 
+    tough = Column(Integer)
+    weird = Column(Integer) 
+    luck = Column(Integer)
+    harm = Column(Integer)
+    experience = Column(Integer)
+    pronouns = Column(String)
+    description = Column(Text)
+    notes = Column(Text)
+    
+    
+    
 
     # email = Column(String, unique=True, index=True)
     # username = Column(String, unique=True, index=True)
